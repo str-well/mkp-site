5bf8061825dbeb32e54636dc7ddce52699eb3676
well <well@m23.studio>
2023-04-11 13:33:45 -0300

    teste

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,10 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Link } from "@chakra-ui/react";
 
 export const Home = () => (
   <Box>
     <h1>Home</h1>
+ <Link>
+      <a href="/login">Login</a>
+ </Link>
   </Box>
 );
